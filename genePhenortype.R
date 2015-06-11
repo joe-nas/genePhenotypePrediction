065ca5314f42663fa81bc4c37b007ffd6c0096a6
J Falck <jfalck@lilo3.science.ru.nl>
2015-06-11 07:07:46 +0200

    functional programming

--- genePhenortype.R
+++ genePhenortype.R
@@ -1,6 +1,9 @@
 library(GEOmetadb)
 library(GEOquery)
 library(plyr)
+library(data.table)
+library(caret)
+library(doMC)
 # download database
 # if(!file.exists('GEOmetadb.sqlite')) getSQLiteFile()
 
@@ -50,12 +53,13 @@ downLoadGDS <- function(gds, download_dest = ".", method = "dl" ){
 # downloaden
 library(doMC)
 registerDoMC(4)
-expression_data <- llply(seq_along(gds), function(i){
+expression_data <- llply(rev(seq_along(gds)), function(i){
   cat(gds[[i]]$gpl,":")
   alply(gds[[i]], 1, function(y){
     cat(y$sample_count," ")
     downLoadGDS(y,".","d")
 }, .inform = T,.parallel = T)})
+
 
 soft2dt <- function(filepath){
   require(data.table)
@@ -68,12 +72,14 @@ soft2dt <- function(filepath){
                              rep("numeric",gsmcolumns)), 
               na.strings = 'null', sep="\t", header = T,
               integer64 = "double",
-              verbose = T)[, .SD,, c("ID_REF","IDENTIFIER")]
+              verbose = F)[, .SD,, c("ID_REF","IDENTIFIER")]
+  cat(".")
   return(dt)
 }
 
-softpath <- function(workingdir,gdsline){
-  sprintf("%s/%s/%s/%s/%s.soft.gz", workingdir, gdsline$sample_organism, gdsline$gpl, gdsline$value_type, gdsline$gds)
+softpath <- function(x,i,workingdir,gdsline){
+  x$gds[[i]] <- sprintf("%s/%s/%s/%s/%s.soft.gz", workingdir, gdsline$sample_organism, gdsline$gpl, gdsline$value_type, gdsline$gds)
+  invisible()
 }
 
 gpldtpath <- function(workingdir, gdsline){
@@ -85,47 +91,143 @@ savedt <- function(gpldtpath, dt){
   write.table(dt ,file = gpldtpath, col.names = T, row.names = F, sep = ",")
 }
 
-gdspaths <- Map(function(x)softpath(".",x),gds)
-fexists <- Map(file.exists,gdspaths)
-gdspaths <- Filter(length,Map(function(x) gdspaths[[x]][fexists[[x]]],seq_along(gdspaths)))
-
-registerDoMC(3)
-gdspaths <- gdspaths[107:111]
-res <- Map(function(i) Reduce(function(x,y) x[y],Map(soft2dt,gdspaths[[i]])),1:length(gdspaths))
-# res.copy <- res
-# laply(res,dim)
-# res <- res[[2]]
-
-#impute.knn(as.matrix(res[,(.SD),.SDcols = grep("GSM",names(res))]))
-library(caret)
-library(lattice)
-
+registerDoMC(1)
+Sys.setenv(OPENBLAS_NUM_THREADS = 4)
+Sys.getenv("OPENBLAS_NUM_THREADS")
 preprocessData <- function(dt){
+  require(caret)
   dt_dim <- dim(dt)
-  PreProcValues <- preProcess(dt[,(.SD),.SDcols = grep("GSM",names(dt))], method = c("center","scale","medianImpute"))
-  transformedData <- data.table(predict(PreProcValues, dt[,(.SD),.SDcols = grep("GSM",names(dt))]))
-  dat_svd <- svd(transformedData, nu = nrow(transformedData),nv = ncol(transformedData))
-  var_explained <- data.frame(PCs = 1:length(dat_svd$d), var = cumsum(dat_svd$d/sum(dat_svd$d)))
-  print(var_explained)
-  n_pcs <- predict(lm(PCs~poly(var,2,raw=T),var_explained[1:nrow(var_explained),]),
-                 newdata = data.frame(var=1))
-  dat_reduced <- data.table(dat_svd$u[,n_pcs] %*% diag(dat_svd$d[n_pcs], length(n_pcs), length(n_pcs)) %*% t(dat_svd$v[,n_pcs]))
-  return(list(var_explained, data.table(dt[,list(ID_REF,IDENTIFIER)],
-                                        dat_reduced, 
-              key = c("ID_REF","IDENTIFIER"))))
+  PreProcValues <- preProcess(dt[,(.SD),.SDcols = grep("GSM",names(dt))], 
+                              method = c("center","scale","medianImpute"))
+  svd_data <- svd(
+    predict(PreProcValues, dt[,(.SD),.SDcols = grep("GSM",names(dt))]),
+    nu = dt_dim[1], nv = dt_dim[2])
+  var_explained <- data.frame(PCs = 1:length(svd_data$d), Var = cumsum(svd_data$d/sum(svd_data$d)))
+  nfit <- round(log2(length(var_explained$PCs)))
+  so_model <- lm(PCs~poly(Var,2,raw=T), data = var_explained[1:nfit,])
+  n_pcs <- predict(so_model, newdata = data.frame(Var=1))
+  n_pcs <- 1:round(n_pcs)
+  dat_reduced <- data.table(svd_data$u[,n_pcs] %*% diag(svd_data$d[n_pcs], length(n_pcs), length(n_pcs)))
+  return(list(var_explained = var_explained, 
+              so_model = so_model,
+              data.table(
+                dt[,list(ID_REF,IDENTIFIER)], 
+                dat_reduced, 
+                key = c("ID_REF","IDENTIFIER")) 
+              ))
 }
 
-result <- preprocessData(res[[2]])
-gc()
 
-xyplot(var~PCs,result$S.cumexplvar,panel = function(x,y,...){
-  panel.xyplot(x,y,...)
-  nfit <- round(log2(length(result$S.cumexplvar$PCs)))
-  tmp <- predict(lm(PCs~poly(var,2,raw=T),result$S.cumexplvar[1:nfit,]),data.frame(var=y))
-  res <- predict(lm(PCs~poly(var,2,raw=T),result$S.cumexplvar[1:nfit,]),data.frame(var=1))
-  panel.xyplot(tmp,y, type = "l")
-  panel.abline(v = res)
+library(lattice)
+xyplot(Var~PCs, data = result$var_explained,
+       so_model = result$so_model,
+       panel = function(x,y,so_model,...){
+         target_pc <- round(predict(so_model,data.frame(Var=1)))
+         xso <- predict(so_model, newdata = data.frame(Var = result$var_explained$Var))
+         panel.xyplot(x,y,...)
+         panel.xyplot(xso,y, ..., type = "l")
+         panel.abline(v = target_pc)
 })
+
+
+
+
+
+
+e <- new.env()
+e$gds <- gds
+
+modify <- function(x, i, arg){
+  x$gds[[i]] <- arg
+  invisible()
+}
+
+modifyl <- function(x,list_name, arg){
+  x[[list_name]] <- arg
+  invisible()
+}
+
+l_ply(seq_along(e$gds), function(i){
+  softpath(e,i,".",e$gds[[i]])
+  modify(e, i, e$gds[[i]][file.exists(e$gds[[i]])])
+  invisible()
+  })
+length(e$gds)
+
+modifyl(e,"gds",Filter(length, e$gds))
+
+registerDoMC(12)
+l_ply(seq_along(e$gds)[1:5], function(i){
+  modify(e,i,llply(e$gds[[i]], soft2dt, .parallel = T))
+  invisible()
+})
+
+
+l_ply(seq_along(e$gds)[1:5], function(i){
+  modify(e,i, Reduce(function(x,y) x[y], e$gds[[i]]))
+  invisible()
+})
+
+modifyl(e,"gds", Filter(function(x) dim(x)[2]>50, e$gds))
+parent.env(e)
+
+
+
+library(caret)
+testdat <- e$gds[[1]]
+
+Sys.setenv(OMP_NUM_THREADS=6,OPENBLAS_NUM_THREADS=6)
+
+dtProcessing <- function(dt){
+  preproc <- preProcess(dt[,3:ncol(dt),with=F], 
+                        method = c("center","scale","medianImpute"))
+  dt[,3:ncol(dt) := data.table(predict(preproc,dt[,3:ncol(dt),with=F])), with=F]
+  dt_svd <- svd(as.matrix(dt[,3:ncol(dt), with = F]),nu = nrow(dt),nv = ncol(dt)-2)
+  var_explained <- data.table(PCs = 1:length(dt_svd$d), Var = cumsum(dt_svd$d/sum(dt_svd$d)))
+  pcs_fit <- round(log2(length(var_explained$PCs)))
+  fit <- lm(PCs~poly(Var,2,raw=T), data = var_explained[1:pcs_fit,])
+  n_pcs <- predict(fit, newdata = data.frame(Var=1))
+  n_pcs <- 1:round(n_pcs)
+  dt[,3:ncol(dt) := NULL]
+  dt[data.table(dt_svd$u[,n_pcs] %*% diag(dt_svd$d[n_pcs], length(n_pcs), length(n_pcs)))]
+  
+}
+
+dtProcessing(testdat)
+
+
+testpreproc <- preProcess(testdat[,3:ncol(testdat),with=F], 
+                          method = c("center","scale","medianImpute"))
+testmat <- as.matrix(testdat[,3:ncol(testdat) := data.table(predict(testpreproc,testdat[,3:ncol(testdat),with=F])), with=F][,3:ncol(testdat),with=F])
+
+mysvd <- svd(testmat, nu = nrow(testmat), nv = ncol(testmat))
+
+
+
+
+
+
+
+
+
+
+
+
+grep("GSM",names(testdat),value = T)
+
+l_ply(seq_along(e$gds)[1:3], function(i){
+  modify(e,i,preprocessData(e$gds[[i]]))
+  invisible()
+}, .progress = "text")
+
+
+
+
+
+
+
+
+
 
 
 res[,lapply(.SD, function(x) is.na(x))), .SDcols = names(res)]
