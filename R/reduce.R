2b5731c339b040c45f4ef351a0f02e9a36be43f1
J Falck <jfalck@lilo3.science.ru.nl>
2015-06-15 02:09:08 +0200

    corrected description

--- R/reduce.R
+++ R/reduce.R
@@ -1,14 +1,11 @@
 #'@export
 Expr$methods(
-  reduce = function(n_pcs = FALSE){
+  reduce = function(){
     gdssvd <- svd(gdsData[,3:ncol(gdsData), with = F], nv = ncol(gdsData)-2, nu = 0)
     var_explained <<- data.frame(PCs = 1:length(gdssvd$d),
                                  Var = cumsum(gdssvd$d/sum(gdssvd$d)))
-    print(var_explained)
-    if(!n_pcs){
-      slope <- diff(var_explained[,"Var"])/diff(var_explained[,"PCs"])
-      n_pcs <<- max(which(slope > quantile(slope,.85)))
-    }
+    slope <- diff(var_explained[,"Var"])/diff(var_explained[,"PCs"])
+    n_pcs <<- max(which(slope > quantile(slope,.85)))
     red <- quote((as.matrix(gdsData[,3:ncol(gdsData),with=F]) %*% t(gdssvd$v))[,1:n_pcs])
     gdsRed <<- data.table(gdsData[,1:2, with=F],eval(red))
   }
